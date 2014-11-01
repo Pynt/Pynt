fbeb0d615e2da8020e3694cc81649fc05f4aef94
Matthew Kaufer <matthew@kaufer.org>
2014-11-01 12:38:05 -0400

    Run python in meteor, bitches!

---
+++ client/pythonInterpreting.js
@@ -0,0 +1,37 @@
+function outf(text) { 
+    var mypre = document.getElementById("output"); 
+    mypre.innerHTML = mypre.innerHTML + text; 
+} 
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+ 
+// Here's everything you need to run a python program in skulpt
+// grab the code from your textarea
+// get a reference to your pre element for output
+// configure the output function
+// call Sk.importMainWithBody()
+function runit() { 
+   var prog = document.getElementById("yourcode").value; 
+   var mypre = document.getElementById("output"); 
+   mypre.innerHTML = ''; 
+   Sk.pre = "output";
+   Sk.configure({output:outf, read:builtinRead}); 
+   try {
+      eval(Sk.importMainWithBody("<stdin>",false,prog)); 
+   }
+   catch(e) {
+       alert(e.toString())
+   }
+} 
+
+Template.pyDraw.events({
+    'click #runPython' : function(){
+      runit();
+    },
+    'touchend #runPython' : function(){
+      runit();
+    }
+});

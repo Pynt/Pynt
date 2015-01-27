2061a14cfe2f7c77d753479acf8029afa785b168
Rushi Shah <2016rshah@gmail.com>
2015-01-27 17:06:19 -0500

    Added method to insert code snippet

--- server/server.js
+++ server/server.js
@@ -1,1 +1,7 @@
 dataStream = new Meteor.Stream('data');
+
+Meteor.methods({
+	'saveSnippet':function(codeSnippet, snippetName, userId){
+		Snippets.insert({code:codeSnippet, name:snippetName, owner:userId})
+	}
+})
